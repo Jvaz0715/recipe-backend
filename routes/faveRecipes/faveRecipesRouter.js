6920db6cfbc1d1521b9e08d4fe2b09a27af36759
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-14 11:43:47 -0400

    in faveRecipesRouter.js, changed line-3 require.../jwtMiddleware to .../jwtMiddleWare as it was not matching the file name. Git pull into aws env to verify this is no longer showing as an error

--- routes/faveRecipes/faveRecipesRouter.js
+++ routes/faveRecipes/faveRecipesRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const jwtMiddleware = require("../utils/jwtMiddleware");
+const jwtMiddleware = require("../utils/jwtMiddleWare");
 
 const {
     addRecipe,
